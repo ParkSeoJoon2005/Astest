3ae244bbe8c6f8dd82cadfe5c272f793b363bcdb
GO Master Hub YT <89635773+GOMasterHub@users.noreply.github.com>
2021-10-21 13:46:35 +0530

    Update MusicPlay.py

--- Natsuki/modules/MusicPlay.py
+++ Natsuki/modules/MusicPlay.py
@@ -1,21 +1,21 @@
-#    Project By TeamNatsuki
+#    Project By TeamGOMaster
 
 __help__ = """
-*  • Groups and channels Voice Chat Music Play 🎧  *
-1) Make bot admin (Group and in channel if use cplay)
-2) Start a voice chat
-3) Try /play [song name] for the first time by an admin
-4) If userbot joined enjoy music, If not add @NatsukiHelper to your group and retry
+*  • Chats and Channels Voice Chat Music Play 🎧  *
+1) Make Bot Admin
+2) Start a Voice Chat
+3) Try /play [Song Name] for the first time by an Admin
+4) If Userbot joined enjoy Music, If not add @NatsukiHelper to Chat and Retry
 * ✨For Channel Music Play✨ *
-1) Make @TheNatsukiBot admin of your channel 
-2) Send /userbotjoinchannel in linked group
-3) Now send commands in linked group 
-* ✨Groups Music Play✨ *
-• /play: Play song using youtube music
-• /play [yt url] : Play the given yt url
-• /play [reply yo audio]: Play replied audio
-• /dplay: Play song via deezer
-• /splay: Play song via jio saavn
+1) Make @TheNatsukiBot Admin of Channel 
+2) Send /userbotjoinchannel in linked Chat
+3) Now send Commands in linked Chat
+* ✨Chat Music Play✨ *
+• /play: Play song using YouTube Music
+• /play [YT url] : Play the given YT url
+• /play [reply to Audio]: Play replied Audio
+• /dplay: Play song via Deezer
+• /splay: Play song via Jio Saavn
  
  * ✨Playback✨ *
 • /player: Open Settings menu of player
@@ -25,11 +25,11 @@ __help__ = """
 • /end: Stops media playback
 • /current: Shows the current Playing track
 • /playlist: Shows playlist
-Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group. 
+Player cmd and all other commands except /play, /current  and /playlist  are only for Admins of the Chat. 
 * ✨Channel Music Play✨ *
 • /cplay [song name] - play song you requested
-• /cdplay [song name] - play song you requested via deezer
-• /csplay [song name] - play song you requested via jio saavn
+• /cdplay [song name] - play song you requested via Deezer
+• /csplay [song name] - play song you requested via Jio Saavn
 • /cplaylist - Show now playing list
 • /cccurrent - Show now playing
 • /cplayer - open music player settings panel
@@ -37,23 +37,23 @@ Player cmd and all other cmds except /play, /current  and /playlist  are only fo
 • /cresume - resume song play
 • /cskip - play next song
 • /cend - stop music play
-• /userbotjoinchannel - invite assistant to your chat
+• /userbotjoinchannel - invite assistant to Chat
 channel is also can be used instead of c ( /cplay = /channelplay )
   
-*  If you donlt like to play in linked group  *
-1) Get your channel ID.
-2) Create a group with tittle: Channel Music: your_channel_id
-3) Add bot as Channel admin with full perms
-4) Add @TheNatsukiBot to the channel as an admin.
-5) Simply send commands in your group.
+*  If you don't like to play in linked Chat  *
+1) Get channel ID.
+2) Create a Chat with Tittle: Channel Music: your_channel_id
+3) Add Bot as Channel Admin with full permissons
+4) Add the Bot to the channel as an Admin.
+5) Simply send commands in the Chat
  
 * ✨More tools✨ *
-• /admincache: Updates admin info of your group. Try if bot isn't recognize admin
-• /userbotjoin: Invite @NatsukiHelper Userbot to your chat 
+• /admincache: Updates Admin info of your Chat. Try if Bot isn't recognize Admin
+• /userbotjoin: Invite @NatsukiHelper Userbot to Chat 
  
-* 🙋‍♀️Assisten🙋‍♀️ *
+* 🙋‍♀️Assistant🙋‍♀️ *
 @NatsukiHelper
 * 🙋‍ Bot 🙋‍♀️ *
-@TheNatsukiBot
+@GOMasterHubAssistantBot
 """
 __mod_name__ = "Music Play"
