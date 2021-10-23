105c2e457716a7209cee51b3a0a7cbf4e38dae51
GO Master Hub YT <89635773+GOMasterHub@users.noreply.github.com>
2021-10-23 15:06:14 +0530

    Update cust_filters.py

--- Natsuki/modules/cust_filters.py
+++ Natsuki/modules/cust_filters.py
@@ -613,7 +613,7 @@ __help__ = """
  ✪ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
-doin?
+doing?
  Separate diff replies by `%%%` to get random replies
  *Example:* 
  `/filter "filtername"
